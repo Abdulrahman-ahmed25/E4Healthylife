799f2cf44193c4a080d99cd9a951788ac8ae79ce
Abdulrahman Ahmed <76537136+Abdulrahman-ahmed25@users.noreply.github.com>
2021-07-17 00:41:16 +0200

    Update views.py

--- account/views.py
+++ account/views.py
@@ -11,31 +11,7 @@ from rest_framework.response import Response
 from rest_framework import status, filters, mixins, generics,viewsets
 
 
-# def register_view(request, *args, **kwargs):
-# 	user = request.user
-# 	if user.is_authenticated: 
-# 		return HttpResponse("You are already authenticated as " + str(user.mobile))
 
-# 	context = {}
-# 	if request.POST:
-# 		form = RegistrationForm(request.POST)
-# 		if form.is_valid():
-# 			form.save()
-# 			mobile = form.cleaned_data.get('mobile')
-# 			raw_password = form.cleaned_data.get('password1')
-# 			account = authenticate(mobile=mobile, password=raw_password)
-# 			login(request, account)
-# 			destination = kwargs.get("next")
-# 			if destination:
-# 				return redirect(destination)
-# 			return redirect('home')
-# 		else:
-# 			context['registration_form'] = form
-
-# 	else:
-# 		form = RegistrationForm()
-# 		context['registration_form'] = form
-# 	return render(request, 'account/register.html', context)
 def register_view(request):
 	return render(request, 'account/firstregister.html',{})
 def buyer_register(request, *args, **kwargs):
